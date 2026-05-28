fc9b830b06787c473f06a8bd637dae3402dfd342
aogwel-svg <aogwel@gmail.com>
2026-05-28 14:24:06 +0300

    test minimal homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,31 +1,7 @@
-import HeroSection from "@/components/hero-section"
-import RecentSermons from "@/components/recent-sermons"
-import SermonSection from "@/components/sermon-section"
-import VisitSection from "@/components/visit-section"
-import AppSection from "@/components/app-section"
-
 export default function Page() {
   return (
-    <main className="flex flex-col">
-      <div className="min-h-screen">
-        <HeroSection />
-      </div>
-
-      <div className="py-10 md:py-16">
-        <SermonSection />
-      </div>
-
-      <div className="py-10 md:py-16">
-        <RecentSermons />
-      </div>
-
-      <div className="py-10 md:py-20">
-        <AppSection />
-      </div>
-
-      <div className="py-10 md:py-20">
-        <VisitSection />
-      </div>
-    </main>
+    <div style={{ padding: 40 }}>
+      VERCEL WORKING
+    </div>
   )
 }
