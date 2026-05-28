fc6d2f91ca293d69ee3705f02bc00fcebaaf74cc
aogwel-svg <aogwel@gmail.com>
2026-05-28 10:59:06 +0300

    remove OfflineDetector to fix build

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next"
 import { Geist, Geist_Mono } from "next/font/google"
 import "./globals.css"
-import OfflineDetector from "@/components/OfflineDetector"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -34,7 +33,6 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        <OfflineDetector />
         {children}
       </body>
     </html>
