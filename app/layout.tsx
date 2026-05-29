d2f4e55c73e6ab0387068d9d0dbf293ac836c103
aogwel-svg <aogwel@gmail.com>
2026-05-29 04:10:16 +0300

    add pwa icons

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,10 @@ import "./globals.css"
 export const metadata = {
   title: "Church App",
   description: "Church Landing Page",
+  icons: {
+    icon: "/favicon.ico",
+    apple: "/apple-touch-icon.png",
+  },
 }
 
 export default function RootLayout({
